5e4edd9035e90e8faea58a05ae580e15d8b8059e
ronn <ronnie@nextdigital.es>
2020-09-26 16:52:10 +0200

    Add reducer for home

--- src/domain/home/reducer/home.js
+++ src/domain/home/reducer/home.js
@@ -1,6 +1,6 @@
 const initialState = {}
 
-export const homeReducer = (state = initialState, action) => {
+export default (state = initialState, action) => {
     switch (action.type) {
         default:
             return state
