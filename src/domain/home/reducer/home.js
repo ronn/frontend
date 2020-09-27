f4197466585b3eff82511af5290ab6e043a7c26a
ronn <ronnie@nextdigital.es>
2020-09-27 15:47:23 +0200

    Add home reducer

--- src/domain/home/reducer/home.js
+++ src/domain/home/reducer/home.js
@@ -1,7 +1,49 @@
-const initialState = {}
+import Request from "../../shared/model/Request";
+
+const GETTING_POSTS = "GETTING_POSTS"
+const ERROR_POSTS = "ERROR_POSTS"
+const SUCCESS_POSTS = "SUCCESS_POSTS"
+
+export const gettingPosts = () => ({
+    type: GETTING_POSTS
+})
+
+export const errorPosts = errorMessage => ({
+    type: ERROR_POSTS,
+    errorMessage
+})
+
+export const successPosts = posts => ({
+    type: SUCCESS_POSTS,
+    posts
+})
+
+const initialState = {
+    posts: [],
+    postsRequest: Request.NONE,
+    errorMessage: ""
+}
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case GETTING_POSTS:
+            return {
+                ...state,
+                postsRequest: Request.PENDING
+            }
+        case ERROR_POSTS:
+            return {
+                ...state,
+                postsRequest: Request.ERROR,
+                errorMessage: action.errorMessage
+            }
+        case SUCCESS_POSTS:
+            return {
+                ...state,
+                postsRequest: Request.SUCCESS,
+                errorMessage: "",
+                posts: action.posts
+            }
         default:
             return state
     }
